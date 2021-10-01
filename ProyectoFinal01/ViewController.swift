74a96fbc0bfd46718debfdfb1b136c245b159c10
Juan Carlos OMB <A01656520@itesm.mx>
2021-09-30 21:27:53 -0500

    Initial Commit

---
+++ ProyectoFinal01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoFinal01
+//
+//  Created by Juan Carlos OMB on 30/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
