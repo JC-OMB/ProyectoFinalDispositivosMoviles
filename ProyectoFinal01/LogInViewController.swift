889b8d3ad5f4d6afdad437ffe40f20d48104fd08
165rmv <47989205+165rmv@users.noreply.github.com>
2021-11-06 00:12:46 -0600

    ya se pueden registrar cambios, pero login no funciona al cambiar de pantalla

---
+++ ProyectoFinal01/LogInViewController.swift
@@ -0,0 +1,85 @@
+//
+//  LogInViewController.swift
+//  ProyectoFinal01
+//
+//  Created by Jose Gerardo Moran Hernandez on 05/11/21.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var mail: UITextField!
+    
+    @IBOutlet weak var pass: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func registrarse(_ sender: Any) {
+        if let email = mail.text, let password = pass.text{
+            Auth.auth().createUser(withEmail: email, password: password){
+                (result, error) in
+                if let result = result, error == nil{
+                    // funciona el registro
+                    // alert que menciona que fue exitoso el registro
+                    let successController = UIAlertController(title: "Exito", message: "Exito al registrar", preferredStyle: .alert)
+                    successController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(successController, animated: true, completion: nil)
+                }else{
+                    // no funciono el registro
+                    // alert que menciona que no fue exitoso el registro
+                    let alertController = UIAlertController(title: "Error", message: "Error al generar registro", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    
+    @IBAction func logIn(_ sender: Any) {
+        if let email = mail.text, let password = pass.text{
+            Auth.auth().signIn(withEmail: email, password: password){
+                (result, error) in
+                if let result = result, error == nil{
+                    // funciona log in
+                    /*
+                    self.navigationController?.pushViewController(<#T##viewController: UIViewController##UIViewController#>, animated: <#T##Bool#>)
+                    */
+                    // alert que menciona que fue exitoso el log in
+                    
+                    let successController = UIAlertController(title: "Exito", message: "Login Exitoso", preferredStyle: .alert)
+                    successController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(successController, animated: true, completion: nil)
+ //*/
+                    //
+                    // self.performSegue(withIdentifier: "FirstSegue", sender: self) //<- revisar
+                }else{
+                    // no funciono el log in
+                    // alert que menciona que no fue exitoso el log in
+                    let alertController = UIAlertController(title: "Error", message: "Error al hacer el Login", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ProyectoFinal01/ViewController.swift
+++ ProyectoFinal01/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
     @IBOutlet weak var Nombre: UITextField!
     @IBOutlet weak var Correo: UITextField!
     @IBOutlet weak var Password: UITextField!
     @IBOutlet weak var Telefono: UITextField!
+    //manco :v
+   /*
+    func validateFields() -> String?{
+        if Nombre.text?.components(in: .whitespacesAndNewLines)=="" || Correo.text?.components(in: .whitespacesAndNewLines)=="" || Password.text?.components(in: .whitespacesAndNewLines)=="" || Telefono.text?.components(in: .whitespacesAndNewLines)=="" {
+            
+        }
+    }
+    */
+    @IBAction func CrearCuenta(_ sender: Any) {
+        if let email = Correo.text, let password = Password.text{
+            Auth.auth().createUser(withEmail: email, password: password){
+                (result, error) in
+                if let result = result, error == nil{
+                    //exito
+                    
+                }else{
+                    //error
+                    let alertController = UIAlertController(title: "Error", message: "Error al crear al usuario", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
