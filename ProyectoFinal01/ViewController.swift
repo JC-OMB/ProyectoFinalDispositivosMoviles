73234694a7c97e6f3be19e68bccee4fcd9542e1d
DVilla2397 <49038173+DVilla2397@users.noreply.github.com>
2021-11-07 20:57:29 -0600

    NuevosCambios2

--- ProyectoFinal01/ViewController.swift
+++ ProyectoFinal01/ViewController.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var users = [contactos]()
-    
+
     @IBOutlet weak var Nombre: UITextField!
     @IBOutlet weak var Correo: UITextField!
     @IBOutlet weak var Password: UITextField!
@@ -20,31 +18,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
-        let user = users[indexPath.row]
-        
-        cell.textLabel?.text = user.name
-        cell.detailTextLabel?.text = String(user.phone)
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        AlertService.updateUser(in: self){
-            
-        }
-    }
-    
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowat indexPath: IndexPath){
-        guard editingStyle == .delete else {return}
-    }
-
 
 }
 
